e44a6e8a71cb1f78272c9321ede09734a4cb3445
Dhruv Batra <dhruvbatra2@gmail.com>
2020-08-22 21:29:01 -0400

    fixed js

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     #security
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'g3@hn!sTC%LtUc@c9Vc%ZBkb^qSD3Hq8w^ZJUZT&j%&n7c5cZD#@R'
+    basedir = os.path.abspath(os.path.dirname(__file__))
