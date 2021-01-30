56b1ee0e84c395073e0b149ee1dc5135f5159b32
Dhruv Batra <dhruvbatra2@gmail.com>
2021-01-30 00:54:35 -0500

    file org

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-set FLASK_APP=lodi.py
-set FLASK_DEBUG=1
--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    #security
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'g3@hn!sTC%LtUc@c9Vc%ZBkb^qSD3Hq8w^ZJUZT&j%&n7c5cZD#@R'
-    basedir = os.path.abspath(os.path.dirname(__file__))
